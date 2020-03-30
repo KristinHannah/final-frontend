cd0acbe6d77fcc07cd47bd3f1a3e14574ee8612c
KristinHannah <kristinhannahk@gmail.com>
2020-03-29 23:33:58 -0400

    adding vendor type to reducer and action

--- src/actions/addVendorType.js
+++ src/actions/addVendorType.js
@@ -1,6 +1,17 @@
 export const addVendorType = (data) => {
-    debugger;
     return (dispatch) => {
-
+        fetch('http://localhost:3000/api/v1/vendor_types', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify(data)
+        })
+            .then(resp => resp.json())
+            .then(data => dispatch({
+                type: 'ADD_VENDORTYPE',
+                payload: data
+            }))
     }
 }
--- src/reducers/vendorTypeReducer.js
+++ src/reducers/vendorTypeReducer.js
@@ -3,6 +3,8 @@ export default function vendorTypeReducer(state = { vendorTypes: [] }, action) {
     switch (action.type) {
         case 'FETCH_VENDORTYPES':
             return { vendorTypes: action.payload.data }
+        case 'ADD_VENDORTYPE':
+            return { ...state, vendorTypes: [...state.vendorTypes, action.payload.data] }
         default:
             return state
     }
