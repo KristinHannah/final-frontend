4ad0bc150bed9a84885ab26fc7954487627e6ccb
KristinHannah <kristinhannahk@gmail.com>
2020-04-23 10:31:17 -0400

    adding validations and errors messages on vendor input

--- front-end/src/components/VendorInput.js
+++ front-end/src/components/VendorInput.js
@@ -9,7 +9,9 @@ class VendorInput extends React.Component {
         location: '',
         availability: '',
         quote: '',
-        hired: 'false'
+        hired: 'false',
+
+        errors: []
     }
 
     handleChange = (event) => {
@@ -18,21 +20,49 @@ class VendorInput extends React.Component {
         })
     }
 
+    validate = (name, location, availability, quote) => {
+        const errors = []
+        if (name.length === 0) {
+            errors.push("Name can't be empty")
+        }
+        if (location.length === 0) {
+            errors.push("Location can't be empty")
+        }
+        if (availability.length === 0) {
+            errors.push("Availability can't be empty")
+        }
+        if (quote.length === 0) {
+            errors.push("Quote can't be empty")
+        }
+        return errors;
+    }
+
     handleSubmit = (event) => {
         event.preventDefault()
-        this.props.addVendor(this.state, this.props.vendorType.id)
+        const { name, location, availability, quote } = this.state
+        const errors = this.validate(name, location, availability, quote);
+        if (errors.length > 0) {
+            this.setState({ errors });
+            return;
+        }
+        else {
+            this.props.addVendor(this.state, this.props.vendorType.id)
+        }
         this.setState({
             name: '',
             location: '',
             availability: '',
-            quote: ''
+            quote: '',
+            hired: 'false'
         })
     }
 
     render() {
+        const { errors } = this.state
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
+                    {errors.map(error => <p key={error}>Error: {error}</p>)}
                     <label>
                         Vendor Name:
                     </label>
